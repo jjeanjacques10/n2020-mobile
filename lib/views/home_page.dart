aa504fdbf88dfb2d347ac40b5131112fd580853b
jjeanjacques10 <jjean.jacques10@gmail.com>
2020-06-12 22:34:27 -0300

    Adicionando cards de sugestão

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:n2020mobile/widgets/bot_card_item.dart';
+import 'package:n2020mobile/widgets/suggestion_card_item.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -169,78 +170,15 @@ class _HomePageState extends State<HomePage> {
                   left: width * 0.04, right: width * 0.04, top: 0),
               child: SizedBox(
                 width: width,
-                height: height * 0.47,
+                height: height * 0.37,
                 child: ListView.builder(
-                  itemCount: 1,
-                  itemBuilder: (BuildContext ctx, int index) {
-                    return Card(
-                      clipBehavior: Clip.antiAlias,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      elevation: 5.0,
-                      margin: new EdgeInsets.symmetric(
-                        horizontal: 15.0,
-                        vertical: 6.0,
-                      ),
-                      child: Container(
-                        height: height * 0.16,
-                        width: width * 0.1,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Colors.grey[400],
-                                  blurRadius: 5,
-                                  offset: Offset(0, 2))
-                            ]),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            
-                            Padding(
-                              padding: EdgeInsets.only(
-                                left: width * 0.05,
-                                top: width * 0.04,                                
-                              ),
-                              child: Text(
-                                "Podcast",
-                                style: TextStyle(
-                                    color: Colors.lightBlue[700],
-                                    fontSize: width * 0.042),
-                              ),
-                            ),
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.only(left: width * 0.05, top: width * 0.018),
-                                  child: Container(
-                                    width: width * 0.6,
-                                    child: Text(
-                                      "Jovem Nerd",
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                        color: Colors.grey[600],
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: width * 0.06,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: height * 0.008,
-                            )
-                          ],
-                        ),
-                      ),
-                    );
+                  itemCount: 7,
+                  itemBuilder: (ctx, index) {
+                    return SuggestionCardItem(
+                        type: "Podcast",
+                        name: index.toString() + " Jovem Nerd",
+                        height: height,
+                        width: width);
                   },
                 ),
               ),
