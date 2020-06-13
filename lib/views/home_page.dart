e083c439c986915628a6b06420f72d917c9800c8
Vitor Rico <rm80515@fiap.com.br>
2020-06-12 21:21:22 -0300

    Layout home parte 1

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -7,36 +7,180 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var total;
+  var width;
+  var height;
+  final GlobalKey<ScaffoldState> _scafoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: new AppBar(
-        title: Text('Goodbot'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Center(
-            child: BotListCard(
-              image: 'assets/images/logo.jpeg',
-              name: 'Eliza',
-              auth: '',
+      key: _scafoldKey,
+      body: SingleChildScrollView(
+        primary: false,
+        physics: NeverScrollableScrollPhysics(),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  height: height * 0.334,
+                  color: Colors.white,
+                ),
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                      width: double.infinity,
+                      height: height * 0.28,
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue[700],
+                      )),
+                ),
+                Positioned(
+                  top: width * 0.18,
+                  left: width * 0.085,
+                  child: Text(
+                    "Olá, [name]",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: width * 0.07,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: width * 0.07,
+                  right: width * 0.07,
+                  child: Container(
+                    height: height * 0.16,
+                    width: width * 0.1,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey[400],
+                              blurRadius: 5,
+                              offset: Offset(0, 2))
+                        ]),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(
+                            left: width * 0.05,
+                            top: width * 0.04,
+                            bottom: width * 0.02,
+                          ),
+                          child: Text(
+                            "Conversar com",
+                            style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: width * 0.05),
+                          ),
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(left: width * 0.05),
+                              child: Container(
+                                width: width * 0.6,
+                                child: Text(
+                                  "Elisa",
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    color: Colors.lightBlue[700],
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: width * 0.1,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(right: width * 0.04),
+                              child: GestureDetector(
+                                onTap: () {
+                                  /* Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => AddReceita()));
+                                 */
+                                },
+                                child: Container(
+                                  width: width * 0.12,
+                                  height: width * 0.12,
+                                  decoration: BoxDecoration(
+                                      color: Colors.lightBlue[700],
+                                      borderRadius: BorderRadius.circular(50),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey,
+                                          blurRadius: 7,
+                                          offset: Offset(2, 2),
+                                        )
+                                      ]),
+                                  child: Icon(
+                                    Icons.chat,
+                                    size: width * 0.07,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: height * 0.008,
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
             ),
-          ),
-          Center(
-            child: BotListCard(
-              image: 'assets/images/logo.jpeg',
-              name: 'Eliza',
-              auth: '',
+            Padding(
+                padding:
+                    EdgeInsets.only(left: width * 0.04, right: width * 0.04),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 20, left: 22),
+                      child: Text(
+                        "Sugestões:",
+                        style: TextStyle(
+                            color: Colors.grey[600], fontSize: width * 0.04),
+                      ),
+                    ),
+                  ],
+                )),
+                
+            Padding(
+              padding: EdgeInsets.only(
+                  left: width * 0.04, right: width * 0.04, top: 0),
+              child: SizedBox(
+                width: width,
+                height: height * 0.47,
+                child: ListView.builder(
+                  itemCount: 1,
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return null;
+                  },
+                ),
+              ),
             ),
-          ),
-          Text(
-            'Sugestões',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
